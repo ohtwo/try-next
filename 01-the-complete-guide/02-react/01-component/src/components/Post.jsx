4621ab23b66f697bb3e02e1903548a797c43a916
ohtwo <hello@tinygram.dev>
2025-09-12 15:23:48 +0900

    20

--- 01-the-complete-guide/02-react/01-component/src/components/Post.jsx
+++ 01-the-complete-guide/02-react/01-component/src/components/Post.jsx
@@ -1,7 +1,12 @@
+const names = ["Max", "John"];
+
 function Post() {
+  const chosendName = Math.random() > 0.5 ? names[0] : names[1];
+  console.log(chosendName);
+
   return (
     <div>
-      <p>Post Title</p>
+      <p>{chosendName}</p>
       <p>Post Content</p>
     </div>
   );
