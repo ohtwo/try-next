45b2a7bf07885b704cba69882908efadeb2006bf
ohtwo <hello@tinygram.dev>
2025-09-12 18:22:38 +0900

    25

--- 01-the-complete-guide/02-react/01-component/src/components/PostsList.jsx
+++ 01-the-complete-guide/02-react/01-component/src/components/PostsList.jsx
@@ -1,12 +1,16 @@
 import Post from "./Post.jsx";
 import classes from "./PostsList.module.css";
+import NewPost from "./NewPost.jsx";
 
 function PostsList() {
   return (
-    <ul className={classes.posts}>
-      <Post author="Max" body="React.js is awesome!" />
-      <Post author="John" body="John.js is awesome!" />
-    </ul>
+    <>
+      <NewPost />
+      <ul className={classes.posts}>
+        <Post author="Max" body="React.js is awesome!" />
+        <Post author="John" body="John.js is awesome!" />
+      </ul>
+    </>
   );
 }
 
