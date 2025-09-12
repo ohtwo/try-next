40292af0dc2db2a71bdd37c9db5d24d486171584
ohtwo <hello@tinygram.dev>
2025-09-12 23:43:04 +0900

    29

--- 01-the-complete-guide/02-react/01-component/src/components/PostsList.jsx
+++ 01-the-complete-guide/02-react/01-component/src/components/PostsList.jsx
@@ -2,6 +2,7 @@ import Post from "./Post.jsx";
 import classes from "./PostsList.module.css";
 import NewPost from "./NewPost.jsx";
 import { useState } from "react";
+import Modal from "./Modal.jsx";
 
 function PostsList() {
   const [enteredBody, setEnteredBody] = useState("");
@@ -17,10 +18,12 @@ function PostsList() {
 
   return (
     <>
-      <NewPost
-        onBodyChange={bodyChangeHandler}
-        onAuthorChange={authorChangeHandler}
-      />
+      <Modal>
+        <NewPost
+          onBodyChange={bodyChangeHandler}
+          onAuthorChange={authorChangeHandler}
+        />
+      </Modal>
       <ul className={classes.posts}>
         <Post author={enteredAuthor} body={enteredBody} />
         <Post author="John" body="John.js is awesome!" />
