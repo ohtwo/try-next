fc7a717a21db2404b78afad3339c5bae682ba9d0
ohtwo <hello@tinygram.dev>
2025-09-12 23:07:36 +0900

    27

--- 01-the-complete-guide/02-react/01-component/src/components/NewPost.jsx
+++ 01-the-complete-guide/02-react/01-component/src/components/NewPost.jsx
@@ -1,16 +1,20 @@
-import classes from './NewPost.module.css';
+import { useState } from "react";
+import classes from "./NewPost.module.css";
 
 function NewPost() {
+  const [enteredBody, setEnteredBody] = useState("");
+
   function changeBodyHandler(event) {
-    console.log(event.target.value);
+    setEnteredBody(event.target.value);
   }
 
   return (
     <form className={classes.form}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} onChange={changeBodyHandler}/>
+        <textarea id="body" required rows={3} onChange={changeBodyHandler} />
       </p>
+      <p>{enteredBody}</p>
       <p>
         <label htmlFor="name">Your name</label>
         <input type="text" id="name" required />
