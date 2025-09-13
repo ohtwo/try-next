9b2b9beb593399a0a3bb81749f6956a52890bf13
ohtwo <hello@tinygram.dev>
2025-09-13 17:55:20 +0900

    33

--- 01-the-complete-guide/02-react/01-component/src/components/PostsList.jsx
+++ 01-the-complete-guide/02-react/01-component/src/components/PostsList.jsx
@@ -1,34 +1,17 @@
 import Post from "./Post.jsx";
 import classes from "./PostsList.module.css";
 import NewPost from "./NewPost.jsx";
-import { useState } from "react";
 import Modal from "./Modal.jsx";
 
 function PostsList({ isPosting, onStopPosting }) {
-  const [enteredBody, setEnteredBody] = useState("");
-  const [enteredAuthor, setEnteredAuthor] = useState("");
-
-  function bodyChangeHandler(event) {
-    setEnteredBody(event.target.value);
-  }
-
-  function authorChangeHandler(event) {
-    setEnteredAuthor(event.target.value);
-  }
-
   return (
     <>
       {isPosting && (
         <Modal onClose={onStopPosting}>
-          <NewPost
-            onBodyChange={bodyChangeHandler}
-            onAuthorChange={authorChangeHandler}
-            onCancel={onStopPosting}
-          />
+          <NewPost onCancel={onStopPosting} />
         </Modal>
       )}
       <ul className={classes.posts}>
-        <Post author={enteredAuthor} body={enteredBody} />
         <Post author="John" body="John.js is awesome!" />
       </ul>
     </>
