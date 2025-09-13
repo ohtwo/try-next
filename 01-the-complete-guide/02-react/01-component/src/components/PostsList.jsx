0eb2979513d9fa1c2f44ccac984f7485adb38236
ohtwo <hello@tinygram.dev>
2025-09-13 20:30:48 +0900

    34

--- 01-the-complete-guide/02-react/01-component/src/components/PostsList.jsx
+++ 01-the-complete-guide/02-react/01-component/src/components/PostsList.jsx
@@ -2,13 +2,22 @@ import Post from "./Post.jsx";
 import classes from "./PostsList.module.css";
 import NewPost from "./NewPost.jsx";
 import Modal from "./Modal.jsx";
+import { useState } from "react";
 
 function PostsList({ isPosting, onStopPosting }) {
+  const [posts, setPosts] = useState([]);
+
+  function addPostHandler(post) {
+    setPosts((prevPosts) => {
+      return [post, ...prevPosts];
+    });
+  }
+
   return (
     <>
       {isPosting && (
         <Modal onClose={onStopPosting}>
-          <NewPost onCancel={onStopPosting} />
+          <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
         </Modal>
       )}
       <ul className={classes.posts}>
