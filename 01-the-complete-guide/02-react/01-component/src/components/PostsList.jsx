2d5c61cc9b62b34fc5f1a16a9f39153fb3f5edd4
ohtwo <hello@tinygram.dev>
2025-09-14 02:12:23 +0900

    39

--- 01-the-complete-guide/02-react/01-component/src/components/PostsList.jsx
+++ 01-the-complete-guide/02-react/01-component/src/components/PostsList.jsx
@@ -6,12 +6,15 @@ import Modal from "./Modal.jsx";
 
 function PostsList({ isPosting, onStopPosting }) {
   const [posts, setPosts] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
 
   useEffect(() => {
     async function fetchPosts() {
+      setIsFetching(true);
       const response = await fetch("http://localhost:8080/posts");
       const data = await response.json();
       setPosts(data.posts);
+      setIsFetching(false);
     }
 
     fetchPosts();
@@ -35,17 +38,22 @@ function PostsList({ isPosting, onStopPosting }) {
           <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
         </Modal>
       )}
-      {posts.length > 0 && (
+      {!isPosting && posts.length > 0 && (
         <ul className={classes.posts}>
           {posts.map((post) => (
             <Post key={post.id} author={post.author} body={post.body} />
           ))}
         </ul>
       )}
-      {posts.length === 0 && (
+      {!isFetching && posts.length === 0 && (
         <div style={{ color: "white", textAlign: "center" }}>
           <h2>No posts yet.</h2>
           <p>Start adding some post!</p>
+        </div>
+      )}
+      {isFetching && (
+        <div style={{ color: "white", textAlign: "center" }}>
+          <p>Loading posts...</p>
         </div>
       )}
     </>
