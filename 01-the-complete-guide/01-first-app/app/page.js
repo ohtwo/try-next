9e3ffc7835293497e5e4615d3382c373a7f7b323
ohtwo <hello@tinygram.dev>
2025-09-15 18:16:05 +0900

    91

--- 01-the-complete-guide/01-first-app/app/page.js
+++ 01-the-complete-guide/01-first-app/app/page.js
@@ -1,9 +1,15 @@
+import Link from "next/link";
+
 export default function Home() {
+  console.log("Hello from the Home page!");
   return (
     <main>
       <img src="/logo.png" alt="A server surrounded by magic sparkles." />
       <h1>Welcome to this NextJS Course!</h1>
       <p>🔥 Let&apos;s get started! 🔥</p>
+      <p>
+        <Link href="awesome">Awesome</Link>
+      </p>
     </main>
   );
 }
