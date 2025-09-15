82e84752ff9425652a816215b75f9387b6347916
ohtwo <hello@tinygram.dev>
2025-09-16 04:58:19 +0900

    102

--- 01-the-complete-guide/03-foodies-app/components/main-header.jsx
+++ 01-the-complete-guide/03-foodies-app/components/main-header.jsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 
 import logoImg from "@/assets/logo.png";
 import styles from "./main-header.module.css";
@@ -7,7 +8,7 @@ export default function MainHeader() {
   return (
     <header className={styles.header}>
       <Link className={styles.logo} href="/">
-        <img src={logoImg.src} alt="Logo Image" />
+        <Image src={logoImg} alt="Logo Image" priority />
         NextLevel Food
       </Link>
 
