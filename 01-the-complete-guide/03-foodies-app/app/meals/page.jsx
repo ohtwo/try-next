9eaddb0721c38f2b061eaaeb544642376a22683c
ohtwo <hello@tinygram.dev>
2025-09-17 00:22:54 +0900

    108

--- 01-the-complete-guide/03-foodies-app/app/meals/page.jsx
+++ 01-the-complete-guide/03-foodies-app/app/meals/page.jsx
@@ -1,3 +1,26 @@
+import Link from "next/link";
+
+import MealsGrid from "@/components/meals/meals-grid";
+import styles from "./page.module.css";
+
 export default function MealsPage() {
-  return <h1>Meals</h1>;
+  return (
+    <>
+      <header className={styles.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={styles.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={styles.cta}>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={styles.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
+  );
 }
