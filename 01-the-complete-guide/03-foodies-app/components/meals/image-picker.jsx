a521e028937028981e03cd2180e20440da2b1c1b
ohtwo <hello@tinygram.dev>
2025-09-19 06:46:32 +0900

    119

--- 01-the-complete-guide/03-foodies-app/components/meals/image-picker.jsx
+++ 01-the-complete-guide/03-foodies-app/components/meals/image-picker.jsx
@@ -1,20 +1,47 @@
 "use client";
 
-import { useRef } from "react";
+import { useRef, useState } from "react";
+import Image from "next/image";
 
 import styles from "./image-picker.module.css";
 
 export default function ImagePicker({ label, name }) {
+  const [pickedImage, setPickedImage] = useState();
   const imageInput = useRef();
 
   function handlePickButton() {
     imageInput.current.click();
   }
 
+  function handleImageChange(event) {
+    const file = event.target.files[0];
+
+    if (!file) {
+      setPickedImage(null);
+      return;
+    }
+
+    const reader = new FileReader();
+    reader.onload = () => {
+      setPickedImage(reader.result);
+    };
+    reader.readAsDataURL(file);
+  }
+
   return (
     <div className={styles.picker}>
       <label htmlFor={name}>{label}</label>
       <div className={styles.controls}>
+        <div className={styles.preview}>
+          {!pickedImage && <p>No Image picked yet.</p>}
+          {pickedImage && (
+            <Image
+              src={pickedImage}
+              alt="The image selected by the user."
+              fill
+            />
+          )}
+        </div>
         <input
           className={styles.input}
           type="file"
@@ -22,6 +49,7 @@ export default function ImagePicker({ label, name }) {
           accept="image/png, image/jpg"
           name={name}
           ref={imageInput}
+          onChange={handleImageChange}
         />
         <button
           className={styles.button}
